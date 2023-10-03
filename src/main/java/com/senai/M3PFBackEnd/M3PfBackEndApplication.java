79a6a44f1a6225496cd7a6ccaa9966692e4cac34
Kananda Degues <kananda.degues@gmail.com>
2023-10-03 19:52:55 -0300

    Initial Commit

---
+++ src/main/java/com/senai/M3PFBackEnd/M3PfBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.senai.M3PFBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3PfBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3PfBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/M3PFBackEnd/M3PfBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.M3PFBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3PfBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
