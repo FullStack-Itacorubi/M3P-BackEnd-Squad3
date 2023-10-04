785922812774c4fac87aef8a2f77155a30ed2004
Adir Silva Filho <tarukagahobenu@gmail.com>
2023-10-04 11:39:37 -0300

    feat: create UserRepository

---
+++ src/main/java/com/senai/M3PFBackEnd/repositories/UserRepository.java
@@ -0,0 +1,21 @@
+package com.senai.M3PFBackEnd.repositories;
+
+import com.senai.M3PFBackEnd.entities.UserEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    boolean existsByCpf(String cpf);
+
+    boolean existsByEmail(String email);
+
+    boolean existsById(UUID id);
+
+    UserEntity getReferenceById(UUID id);
+
+    void deleteById(UUID id);
+}
