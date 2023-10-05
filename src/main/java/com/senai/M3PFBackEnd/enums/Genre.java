6b7d4a7608958110d2b44012c975b9634393f2ef
Adir Silva Filho <tarukagahobenu@gmail.com>
2023-10-04 22:30:26 -0300

    feat: create enum Genre

--- src/main/java/com/senai/M3PFBackEnd/enums/Genre.java
+++ src/main/java/com/senai/M3PFBackEnd/enums/Genre.java
@@ -1,4 +1,19 @@
 package com.senai.M3PFBackEnd.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// TODO: fazer enum Genre
+@Getter
+@AllArgsConstructor
 public enum Genre {
+    CISGENDER("Cisgênero"),
+    TRANSGENDER("Transgênero"),
+    NONBINARY("Não-binário");
+
+    private final String type;
+
+    /*Genre(String type) {
+        this.type = type;
+    }*/
 }
