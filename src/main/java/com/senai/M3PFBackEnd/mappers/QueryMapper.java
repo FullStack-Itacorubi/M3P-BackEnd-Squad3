9e3934765cbc25a35f66bd1753d7ad2f96ab2585
Marcos Hernandez <marcosh.jimenezh@gmail.com>
2023-10-05 18:44:32 -0300

    feat: create QueryMapper

---
+++ src/main/java/com/senai/M3PFBackEnd/mappers/QueryMapper.java
@@ -0,0 +1,38 @@
+package com.senai.M3PFBackEnd.mappers;
+
+
+import com.senai.M3PFBackEnd.dtos.QueryRequestDto;
+import com.senai.M3PFBackEnd.dtos.QueryRequestPutDto;
+import com.senai.M3PFBackEnd.entities.QueryEntity;
+
+public class QueryMapper {
+    private QueryMapper() {
+    }
+
+    public static QueryEntity map(QueryRequestDto source) {
+        QueryEntity target = new QueryEntity();
+
+        target.setReasonForConsultation(source.reasonForConsultation());
+        target.setConsultationDate(source.consultationDate());
+        target.setConsultationTime(source.consultationTime());
+        target.setProblemDescription(source.problemDescription());
+        target.setPrescriptionMedication(source.prescriptionMedication());
+        target.setDosageAndRecautions(source.dosageAndRecautions());
+
+        return target;
+    }
+
+    public static QueryEntity map(QueryRequestPutDto source) {
+        QueryEntity target = new QueryEntity();
+
+        target.setReasonForConsultation(source.reasonForConsultation());
+        target.setConsultationDate(source.consultationDate());
+        target.setConsultationTime(source.consultationTime());
+        target.setProblemDescription(source.problemDescription());
+        target.setPrescriptionMedication(source.prescriptionMedication());
+        target.setDosageAndRecautions(source.dosageAndRecautions());
+
+        return target;
+    }
+}
+
