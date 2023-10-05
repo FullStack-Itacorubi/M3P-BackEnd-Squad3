2f1093fba6909905e2feca94682259d1dc0fda20
Marcos Hernandez <marcosh.jimenezh@gmail.com>
2023-10-05 18:20:14 -0300

    feat: add Queries at Database with import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.queries(reason_for_consultation, consultation_date, consultation_time, problem_description, prescription_medication, dosage_and_recautions, system_status) VALUES ('Covid-19', '12/07/2023', '10:45:07', 'Paciente com sintomas de covid 19', 'Dipirona Sódica 500mg', 'Dosis 500mg de dipirona por comprimido', true);
