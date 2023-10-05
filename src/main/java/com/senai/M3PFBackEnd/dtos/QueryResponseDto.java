815cb4da5a8379ac8bdbacb66fce32fbbb9775d5
Marcos Hernandez <marcosh.jimenezh@gmail.com>
2023-10-05 18:35:43 -0300

    feat: create QueryResponseDto

---
+++ src/main/java/com/senai/M3PFBackEnd/dtos/QueryResponseDto.java
@@ -0,0 +1,32 @@
+package com.senai.M3PFBackEnd.dtos;
+
+import com.senai.M3PFBackEnd.entities.QueryEntity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record QueryResponseDto(
+        Long id,
+        String reasonForConsultation,
+        LocalDate consultationDate,
+        LocalTime consultationTime,
+        String problemDescription,
+
+        //Atualizar e Vincular com entidade Medicamentos
+        String prescriptionMedication,
+        String dosageAndRecautions,
+        Boolean systemStatus
+) {
+    public QueryResponseDto(QueryEntity queryEntity) {
+        this(
+                queryEntity.getId(),
+                queryEntity.getReasonForConsultation(),
+                queryEntity.getConsultationDate(),
+                queryEntity.getConsultationTime(),
+                queryEntity.getProblemDescription(),
+                queryEntity.getPrescriptionMedication(),
+                queryEntity.getDosageAndRecautions(),
+                queryEntity.getSystemStatus()
+        );
+    }
+}
