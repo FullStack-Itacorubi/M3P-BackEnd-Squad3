f8cd698ff7eecd0b66f3ac9a708277f325cd65c8
Icaro <iba1987@gmail.com>
2023-10-05 20:19:45 -0300

    feat(logs): add function to get logs from most recent

--- src/main/java/com/senai/M3PFBackEnd/repositories/LogsRepository.java
+++ src/main/java/com/senai/M3PFBackEnd/repositories/LogsRepository.java
@@ -1,5 +1,7 @@
 package com.senai.M3PFBackEnd.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.senai.M3PFBackEnd.entities.LogMessage;
 @Repository
 public interface LogsRepository extends JpaRepository<LogMessage, Long> {
 
+    public List<LogMessage> findAllOrderByCreatedAtDesc();
 }
