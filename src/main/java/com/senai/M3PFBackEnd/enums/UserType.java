d8a56fb46a9cf33cf775462fe89587d74cd6fb24
Adir Silva Filho <tarukagahobenu@gmail.com>
2023-10-04 22:31:33 -0300

    refactor: change getter and constructor to annotations

--- src/main/java/com/senai/M3PFBackEnd/enums/UserType.java
+++ src/main/java/com/senai/M3PFBackEnd/enums/UserType.java
@@ -1,17 +1,15 @@
 package com.senai.M3PFBackEnd.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// TODO: trabalhar com os valores
+@Getter
+@AllArgsConstructor
 public enum UserType {
     DOCTOR("Médico"),
     ADMINISTRATOR("Administrador"),
     NURSE("Enfermeiro");
 
     private final String type;
-
-    UserType(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
 }
