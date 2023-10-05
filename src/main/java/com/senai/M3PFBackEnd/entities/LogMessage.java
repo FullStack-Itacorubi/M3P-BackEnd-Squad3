9a6745117c2f742cef014585e3777b9f24ec3b45
Icaro <iba1987@gmail.com>
2023-10-05 17:49:21 -0300

    feat(logs): create log entity

---
+++ src/main/java/com/senai/M3PFBackEnd/entities/LogMessage.java
@@ -0,0 +1,27 @@
+package com.senai.M3PFBackEnd.entities;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "log_message")
+@Table(name = "logs")
+public class LogMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
