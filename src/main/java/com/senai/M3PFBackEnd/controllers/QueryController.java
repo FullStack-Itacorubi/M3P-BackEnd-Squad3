6d8ad26f5b0117cd47ea9bdb72d7df309e03575e
Marcos Hernandez <marcosh.jimenezh@gmail.com>
2023-10-05 18:51:58 -0300

    feat: create QueryController e Endpoints

---
+++ src/main/java/com/senai/M3PFBackEnd/controllers/QueryController.java
@@ -0,0 +1,64 @@
+package com.senai.M3PFBackEnd.controllers;
+
+
+import com.senai.M3PFBackEnd.dtos.QueryRequestDto;
+import com.senai.M3PFBackEnd.dtos.QueryRequestPutDto;
+import com.senai.M3PFBackEnd.dtos.QueryResponseDto;
+import com.senai.M3PFBackEnd.services.QueryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/consultas")
+public class QueryController {
+    @Autowired
+    private QueryService queryService;
+
+
+    @PostMapping
+    public ResponseEntity<QueryResponseDto> registerQuery(
+            @RequestBody @Valid QueryRequestDto newQuery
+    ) {
+        QueryResponseDto query = this.queryService.save(newQuery);
+
+        return new ResponseEntity<>(query, HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("{idQuery}")
+    public ResponseEntity<QueryResponseDto> updateQuery(
+            @PathVariable(name = "idQuery") Long id,
+            @RequestBody @Valid QueryRequestPutDto queryToUpdate
+    ){
+        QueryResponseDto queryUpdated = this.queryService.update(id, queryToUpdate);
+
+        return new ResponseEntity<>(queryUpdated, HttpStatus.OK);
+    }
+
+    @GetMapping("{idQuery}")
+    public ResponseEntity<QueryResponseDto> getQuery(
+            @PathVariable(name = "idQuery") Long id
+    ) {
+        QueryResponseDto query = this.queryService.getOne(id);
+
+        return new ResponseEntity<>(query, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("{idQuery}")
+    public ResponseEntity<String> deleteQuery(
+            @PathVariable(name = "idQuery") Long id
+    ) {
+        this.queryService.delete(id);
+
+        return new ResponseEntity<>(
+                "Consulta excluida com sucesso!",
+                HttpStatus.ACCEPTED
+        );
+    }
+
+}
+
