76f1d9da3c16dab71d06cd7aac6e9d07fd40d628
Adir Silva Filho <tarukagahobenu@gmail.com>
2023-10-04 22:35:18 -0300

    feat: add @Pattern to genre, phone and type at User requests DTOs

--- src/main/java/com/senai/M3PFBackEnd/dtos/User/UserRequestDto.java
+++ src/main/java/com/senai/M3PFBackEnd/dtos/User/UserRequestDto.java
@@ -1,7 +1,5 @@
 package com.senai.M3PFBackEnd.dtos.User;
 
-import com.senai.M3PFBackEnd.enums.UserType;
-
 import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.br.CPF;
 
@@ -10,15 +8,22 @@ public record UserRequestDto(
         String fullName,
 
         @NotBlank(message = "O campo gênero é obrigatório!")
+        @Pattern(
+                regexp = "CISGENDER|TRANSGENDER|NONBINARY",
+                message = "O tipo de ser: CISGENDER, TRANSGENDER ou NONBINARY"
+        )
         String genre,
 
         @NotBlank(message = "O campo CPF é obrigatório!")
         @CPF(message = "O campo CPF é inválido!")
         String cpf,
 
+        // TODO: acertar formato telefone
         @NotBlank(message = "O campo telefone é obrigatório!")
-        // @Pattern(regexp = "\d{8,12}")
-        // @Pattern(regexp = "(\([0-9]{2}\)[0-9]{5}-[0-9]{4})|(\([0-9]{2}\)[0-9]{4}-[0-9]{4})")
+        @Pattern(
+                regexp = "(\\([0-9]{2}\\) 9 [0-9]{4}-[0-9]{4})|(\\([0-9]{2}\\)[0-9]{4}-[0-9]{4})",
+                message = "O campo telefone deve ter o seguinte formato: (00) 9 8765-4321"
+        )
         String phone,
 
         @NotBlank(message = "O campo CPF é obrigatório!")
@@ -29,7 +34,11 @@ public record UserRequestDto(
         @Size(min = 6, message = "O campo senha deve ter no mínimo 6 caracteres!")
         String password,
 
-        @NotNull(message = "O campo tipo é obrigatório!")
-        UserType type
+        @NotBlank(message = "O campo tipo é obrigatório!")
+        @Pattern(
+                regexp = "ADMINISTRATOR|DOCTOR|NURSE",
+                message = "O tipo de ser: ADMINISTRATOR, DOCTOR ou NURSE"
+        )
+        String type
 ) {
 }
--- src/main/java/com/senai/M3PFBackEnd/dtos/User/UserRequestPutDto.java
+++ src/main/java/com/senai/M3PFBackEnd/dtos/User/UserRequestPutDto.java
@@ -1,9 +1,7 @@
 package com.senai.M3PFBackEnd.dtos.User;
 
-import com.senai.M3PFBackEnd.enums.UserType;
-
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserRequestPutDto(
@@ -11,18 +9,26 @@ public record UserRequestPutDto(
         String fullName,
 
         @NotBlank(message = "O campo gênero é obrigatório!")
+        @Pattern(
+                regexp = "CISGENDER|TRANSGENDER|NONBINARY",
+                message = "O tipo de ser: CISGENDER, TRANSGENDER ou NONBINARY"
+        )
         String genre,
 
         @NotBlank(message = "O campo telefone é obrigatório!")
         // @Pattern(regexp = "\d{8,12}")
-        // @Pattern(regexp = "(\([0-9]{2}\)[0-9]{5}-[0-9]{4})|(\([0-9]{2}\)[0-9]{4}-[0-9]{4})")
+        @Pattern(
+                regexp = "(\\([0-9]{2}\\) 9 [0-9]{4}-[0-9]{4})|(\\([0-9]{2}\\)[0-9]{4}-[0-9]{4})",
+                message = "O campo telefone deve ter o seguinte formato: (00) 9 8765-4321"
+        )
         String phone,
 
         @NotBlank(message = "O campo senha é obrigatório!")
         @Size(min = 6, message = "O campo senha deve ter no mínimo 6 caracteres!")
         String password,
 
-        @NotNull(message = "O campo tipo é obrigatório!")
-        UserType type
+        @NotBlank(message = "O campo tipo é obrigatório!")
+        @Pattern(regexp = "ADMINISTRATOR|DOCTOR|NURSE", message = "O tipo de ser: ADMINISTRATOR, DOCTOR ou NURSE")
+        String type
 ) {
 }
