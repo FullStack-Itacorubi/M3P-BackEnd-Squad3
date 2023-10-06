e2ab3d026d2a4a2207c57190d35e8b776f35c1d7
Kananda Degues <kananda.degues@gmail.com>
2023-10-06 17:19:11 -0300

    feat: create Exam Mapper to Put Request and method PUT

--- src/main/java/com/senai/M3PFBackEnd/controllers/ExamController.java
+++ src/main/java/com/senai/M3PFBackEnd/controllers/ExamController.java
@@ -1,16 +1,14 @@
 package com.senai.M3PFBackEnd.controllers;
 
 import com.senai.M3PFBackEnd.dtos.exam.ExamRequestPostDto;
+import com.senai.M3PFBackEnd.dtos.exam.ExamRequestPutDto;
 import com.senai.M3PFBackEnd.dtos.exam.ExamResponseDto;
 import com.senai.M3PFBackEnd.services.ExamService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/exames")
@@ -19,9 +17,16 @@ public class ExamController {
     private ExamService examService;
 
     @PostMapping
-    public ResponseEntity<ExamResponseDto> registerExam(@RequestBody @Valid ExamRequestPostDto newExam){
+    public ResponseEntity<ExamResponseDto> registerExam(@RequestBody @Valid ExamRequestPostDto newExam) {
         ExamResponseDto exam = this.examService.save(newExam);
         return new ResponseEntity<>(exam, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{idExam}")
+    public ResponseEntity<ExamResponseDto> updateExam(@PathVariable(name = "idExam") Long id,
+                                                      @RequestBody @Valid ExamRequestPutDto examToUpdate) {
+        ExamResponseDto examUpdated = this.examService.update(id, examToUpdate);
+        return new ResponseEntity<>(examUpdated, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/senai/M3PFBackEnd/services/ExamService.java
+++ src/main/java/com/senai/M3PFBackEnd/services/ExamService.java
@@ -1,12 +1,15 @@
 package com.senai.M3PFBackEnd.services;
 
 import com.senai.M3PFBackEnd.dtos.exam.ExamRequestPostDto;
+import com.senai.M3PFBackEnd.dtos.exam.ExamRequestPutDto;
 import com.senai.M3PFBackEnd.dtos.exam.ExamResponseDto;
 import com.senai.M3PFBackEnd.entities.ExamEntity;
 import com.senai.M3PFBackEnd.mappers.ExamMapper;
 import com.senai.M3PFBackEnd.repositories.ExamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ExamService {
@@ -16,6 +19,24 @@ public class ExamService {
     public ExamResponseDto save(ExamRequestPostDto newExam) {
         ExamEntity exam = ExamMapper.map(newExam);
         return new ExamResponseDto(this.examRepository.save(exam));
+    }
+
+    private void verifyIsHasId(Long id) {
+        boolean isIdExists = this.examRepository.existsById(id);
+
+        if (!isIdExists) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "O id informado é inválido!"
+            );
+        }
+    }
+
+    public ExamResponseDto update(Long id, ExamRequestPutDto examToUpdate){
+        this.verifyIsHasId(id);
+        ExamEntity exam = ExamMapper.map(examToUpdate);
+        exam.setId(id);
+        return new ExamResponseDto(examRepository.save(exam));
     }
 
 }
