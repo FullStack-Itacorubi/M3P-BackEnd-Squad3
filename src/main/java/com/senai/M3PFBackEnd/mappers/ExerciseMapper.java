e6c369fee535f82a49dd88f40347f7d441ee4e6a
Icaro <iba1987@gmail.com>
2023-10-06 18:32:07 -0300

    feat(exercises): create mapper for update dto

--- src/main/java/com/senai/M3PFBackEnd/mappers/ExerciseMapper.java
+++ src/main/java/com/senai/M3PFBackEnd/mappers/ExerciseMapper.java
@@ -1,6 +1,7 @@
 package com.senai.M3PFBackEnd.mappers;
 
 import com.senai.M3PFBackEnd.dtos.exercises.ExerciseRequestPostDto;
+import com.senai.M3PFBackEnd.dtos.exercises.ExerciseRequestPutDto;
 import com.senai.M3PFBackEnd.entities.ExerciseEntity;
 import com.senai.M3PFBackEnd.enums.exercise.ExerciseType;
 
@@ -15,6 +16,20 @@ public class ExerciseMapper {
         target.setType(ExerciseType.valueOf(source.type()));
         target.setWeeklyAmount(source.weeklyAmount());
         target.setDescription(source.description());
+
+        return target;
+    }
+
+    public static ExerciseEntity map(ExerciseRequestPutDto source) {
+        ExerciseEntity target = new ExerciseEntity();
+
+        target.setName(source.name());
+        target.setDate(source.date());
+        target.setTime(source.time());
+        target.setType(ExerciseType.valueOf(source.type()));
+        target.setWeeklyAmount(source.weeklyAmount());
+        target.setDescription(source.description());
+        target.setStatus(source.status());
 
         return target;
     }
