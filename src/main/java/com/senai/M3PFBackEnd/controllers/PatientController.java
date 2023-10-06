8a0c6e178501dbb9ef7704abfcfc3b3357894a30
Adir Silva Filho <tarukagahobenu@gmail.com>
2023-10-06 09:42:44 -0300

    feat: add update method service in PatientController

--- src/main/java/com/senai/M3PFBackEnd/controllers/PatientController.java
+++ src/main/java/com/senai/M3PFBackEnd/controllers/PatientController.java
@@ -1,16 +1,14 @@
 package com.senai.M3PFBackEnd.controllers;
 
 import com.senai.M3PFBackEnd.dtos.patient.PatientRequestPostDto;
+import com.senai.M3PFBackEnd.dtos.patient.PatientRequestPutDto;
 import com.senai.M3PFBackEnd.dtos.patient.PatientResponseDto;
 import com.senai.M3PFBackEnd.services.PatientService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -21,9 +19,19 @@ public class PatientController {
     @PostMapping
     public ResponseEntity<PatientResponseDto> registerPatient(
             @RequestBody @Valid PatientRequestPostDto newPatient
-            ) {
+    ) {
         PatientResponseDto patient = this.patientService.addPatient(newPatient);
 
         return new ResponseEntity<>(patient, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{idPatient}")
+    public ResponseEntity<PatientResponseDto> updatePatient(
+            @PathVariable(name = "idPatient") Long id,
+            @RequestBody @Valid PatientRequestPutDto patientToUpdate
+    ) {
+        PatientResponseDto patientUpdated = this.patientService.update(id, patientToUpdate);
+
+        return new ResponseEntity<>(patientUpdated, HttpStatus.OK);
     }
 }
