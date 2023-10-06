a2894339d423b083651f567a437543c95b635595
Icaro <iba1987@gmail.com>
2023-10-06 15:46:16 -0300

    fix(exercise): add length to name column

--- src/main/java/com/senai/M3PFBackEnd/entities/ExerciseEntity.java
+++ src/main/java/com/senai/M3PFBackEnd/entities/ExerciseEntity.java
@@ -23,7 +23,7 @@ public class ExerciseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String name;
 
     @Column(nullable = false)
