65aded621fcb4cd4cbe5461d3f77c0c2fbf3ddc2
Marcos Hernandez <marcosh.jimenezh@gmail.com>
2023-10-07 10:31:11 -0300

    feat: add Queries at Database with import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
 INSERT INTO public.queries(reason_for_consultation, consultation_date, consultation_time, problem_description, prescription_medication, dosage_and_recautions, status) VALUES ('Covid-19', '12/07/2023', '10:45:07', 'Paciente com sintomas de covid 19', 'Dipirona Sódica 500mg', 'Dosis 500mg de dipirona por comprimido', true);
+INSERT INTO public.diets(diet_name, diet_date, diet_time, type, description, status) VALUES ('Dieta baixar de peso', '19/09/2023', '08:45:07', 'LOW_CARB', 'Saladas e frutos do mar', true);
