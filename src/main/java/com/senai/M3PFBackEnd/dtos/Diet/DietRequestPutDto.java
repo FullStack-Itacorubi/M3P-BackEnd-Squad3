14d4496ed51a521db10544f66f6fb50134c6c421
Marcos Hernandez <marcosh.jimenezh@gmail.com>
2023-10-07 10:50:54 -0300

    feat: create DietRequestPutDto

---
+++ src/main/java/com/senai/M3PFBackEnd/dtos/Diet/DietRequestPutDto.java
@@ -0,0 +1,41 @@
+package com.senai.M3PFBackEnd.dtos.Diet;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record DietRequestPutDto(
+        @NotBlank(message = "O campo Nome da Dieta é obrigatório!")
+        @Size(min = 5, max = 100, message = "O campo Nome da Dieta deve ter no mínimo 5 e máximo 100 caracteres, respectivamente!")
+        String dietName,
+
+        @NotNull(message = "O campo Data da Dieta é obrigatório!")
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        LocalDate dietDate,
+
+        @NotNull(message = "O campo Horário da Dieta é obrigatório!")
+        @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+        LocalTime dietTime,
+
+        @NotBlank(message = "O campo Tipo de Dieta é obrigatório!")
+        @Pattern(
+                regexp = "LOW_CARB|DASH|PALEOLITHIC|KETOGENIC|DUKAN|MEDITERRANEAN|OTHER",
+                message = "O tipo de ser: LOW CARB, DASH, PALEOLITHIC, KETOGENIC, DUKAN, MEDITERRANEAN ou OTHER"
+        )
+        String type,
+
+        //Atualizar e Vincular com entidade Pacientes
+        @Column(nullable=false)
+        String description,
+
+        @NotNull(message = "O campo status é obrigatório!")
+        Boolean status
+) {
+}
