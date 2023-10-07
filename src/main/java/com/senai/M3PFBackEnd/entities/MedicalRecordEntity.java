c152c233f341cfdb41610d518c1dc9d7e6a812a1
Icaro <iba1987@gmail.com>
2023-10-07 17:16:37 -0300

    fix(medical records): remove column from patient and add no args constructor

--- src/main/java/com/senai/M3PFBackEnd/entities/MedicalRecordEntity.java
+++ src/main/java/com/senai/M3PFBackEnd/entities/MedicalRecordEntity.java
@@ -1,7 +1,6 @@
 package com.senai.M3PFBackEnd.entities;
 
 import java.util.List;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,27 +10,26 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.PreRemove;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity(name = "MedicalRecord")
 @Table(name = "medical_records")
 @Getter
 @Setter
+@NoArgsConstructor
 public class MedicalRecordEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    @Column(nullable = false)
+    @OneToOne(optional = false)
     private PatientEntity patient;
     
     @OneToMany
-    @Column(nullable = true)
     private List<ExamEntity> exams;
 
     @OneToMany
-    @Column(nullable = true)
     private List<ExerciseEntity> exercises;
 
     // TODO: adicionar relacionamentos de consulta e dieta
