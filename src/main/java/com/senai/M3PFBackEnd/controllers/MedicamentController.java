c3f41b06fb15551584ce3d5826d84f9d28903d6a
Adir Silva Filho <tarukagahobenu@gmail.com>
2023-10-08 07:55:56 -0300

    feat: add updateM, getAll, getOne and delet to controller.

--- src/main/java/com/senai/M3PFBackEnd/controllers/MedicamentController.java
+++ src/main/java/com/senai/M3PFBackEnd/controllers/MedicamentController.java
@@ -1,16 +1,17 @@
 package com.senai.M3PFBackEnd.controllers;
 
+import com.senai.M3PFBackEnd.dtos.medicament.MedicamentRequestPutDto;
 import com.senai.M3PFBackEnd.dtos.medicament.MedicamentRequestPostDto;
+import com.senai.M3PFBackEnd.dtos.medicament.MedicamentResponseDto;
 import com.senai.M3PFBackEnd.dtos.medicament.MedicamentResponseDto;
 import com.senai.M3PFBackEnd.services.MedicamentService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicamentos")
@@ -25,5 +26,45 @@ public class MedicamentController {
         MedicamentResponseDto medicament = this.medicamentService.save(newMedicament);
 
         return new ResponseEntity<>(medicament, HttpStatus.CREATED);
+    }
+    
+    @PutMapping("{idMedicament}")
+    public ResponseEntity<MedicamentResponseDto> updateMedicament(
+            @PathVariable(name = "idMedicament") Long id,
+            @RequestBody @Valid MedicamentRequestPutDto medicamentToUpdate
+    ) {
+        MedicamentResponseDto medicamentUpdated = this.medicamentService.update(id, medicamentToUpdate);
+
+        return new ResponseEntity<>(medicamentUpdated, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MedicamentResponseDto>> getAllMedicaments(
+            @RequestParam(name = "nome", required = false) String name
+    ) {
+        List<MedicamentResponseDto> medicamentList = this.medicamentService.getAll(name);
+
+        return new ResponseEntity<>(medicamentList, HttpStatus.OK);
+    }
+
+    @GetMapping("{idMedicament}")
+    public ResponseEntity<MedicamentResponseDto> getMedicament(
+            @PathVariable(name = "idMedicament") Long id
+    ) {
+        MedicamentResponseDto medicament = this.medicamentService.getOne(id);
+
+        return new ResponseEntity<>(medicament, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("{idMedicament}")
+    public ResponseEntity<String> deleteMedicament(
+            @PathVariable(name = "idMedicament") Long id
+    ) {
+        this.medicamentService.delete(id);
+
+        return new ResponseEntity<>(
+                "Medicamento excluído com sucesso!",
+                HttpStatus.ACCEPTED
+        );
     }
 }
