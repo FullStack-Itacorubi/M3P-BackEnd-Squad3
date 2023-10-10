926e9205575c2f21b9ac3428a9269e52756ac0fd
Icaro <iba1987@gmail.com>
2023-10-09 21:07:52 -0300

    fix(exercises): add size messages to dtos

--- src/main/java/com/senai/M3PFBackEnd/dtos/exercises/ExerciseRequestPutDto.java
+++ src/main/java/com/senai/M3PFBackEnd/dtos/exercises/ExerciseRequestPutDto.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 
 public record ExerciseRequestPutDto(
     @NotBlank(message = "O campo nome da série de exercícios é obrigatório!")
-    @Size(min = 5, max = 100)
+    @Size(min = 5, max = 100, message = "O campo nome da série de exercícios deve ter entre 5 a 100 caracteres")
     String name,
 
     @NotBlank(message = "O campo tipo é obrigatório!")
@@ -20,7 +20,7 @@ public record ExerciseRequestPutDto(
     Integer weeklyAmount,
 
     @NotBlank(message = "O campo descrição é obrigatório!")
-    @Size(min = 10, max = 1000)
+    @Size(min = 10, max = 1000, message = "O campo descrição deve ter entre 10 a 1000 caracteres")
     String description,
     
     @NotNull(message = "O campo status é obrigatório!")
