8954f25a743e123439fc277e31f1c24103bd85f7
Marcos Hernandez <marcosh.jimenezh@gmail.com>
2023-10-19 19:48:19 -0300

    refactor: checking medications in mapper

--- src/main/java/com/senai/M3PFBackEnd/mappers/QueryMapper.java
+++ src/main/java/com/senai/M3PFBackEnd/mappers/QueryMapper.java
@@ -1,13 +1,13 @@
 package com.senai.M3PFBackEnd.mappers;
 
-
 import com.senai.M3PFBackEnd.dtos.query.QueryRequestDto;
 import com.senai.M3PFBackEnd.dtos.query.QueryRequestPutDto;
 import com.senai.M3PFBackEnd.entities.QueryEntity;
 
+
 public class QueryMapper {
-    private QueryMapper() {
-    }
+    private QueryMapper() {}
+
 
     public static QueryEntity map(QueryRequestDto source) {
         QueryEntity target = new QueryEntity();
@@ -16,7 +16,11 @@ public class QueryMapper {
         target.setConsultationDate(source.consultationDate());
         target.setConsultationTime(source.consultationTime());
         target.setProblemDescription(source.problemDescription());
-        target.setMedicaments(source.medicaments());
+        target.setMedicaments(
+                source.medicaments() != null ?
+                        source.medicaments() :
+                        null
+        );
         target.setDosageAndRecautions(source.dosageAndRecautions());
 
         return target;
@@ -29,9 +33,14 @@ public class QueryMapper {
         target.setConsultationDate(source.consultationDate());
         target.setConsultationTime(source.consultationTime());
         target.setProblemDescription(source.problemDescription());
-        target.setMedicaments(source.medicaments());
+        target.setMedicaments(
+                source.medicaments() != null ?
+                        source.medicaments() :
+                        null
+        );
         target.setDosageAndRecautions(source.dosageAndRecautions());
         target.setStatus(source.status());
+
 
         return target;
     }
