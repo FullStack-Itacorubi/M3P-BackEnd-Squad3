aefffd23ac543efa874c62d7679cd9c09a6be22c
Icaro Andrade <iba1987@gmail.com>
2023-10-27 23:53:07 -0300

    fix: update verification on create medical record

--- src/main/java/com/senai/M3PFBackEnd/services/MedicalRecordService.java
+++ src/main/java/com/senai/M3PFBackEnd/services/MedicalRecordService.java
@@ -15,6 +15,7 @@ import com.senai.M3PFBackEnd.entities.ExerciseEntity;
 import com.senai.M3PFBackEnd.entities.MedicalRecordEntity;
 import com.senai.M3PFBackEnd.entities.PatientEntity;
 import com.senai.M3PFBackEnd.repositories.MedicalRecordRepository;
+import com.senai.M3PFBackEnd.repositories.PatientRepository;
 
 @Service
 public class MedicalRecordService {
@@ -22,8 +23,11 @@ public class MedicalRecordService {
     @Autowired
     MedicalRecordRepository medicalRecordsRepository;
 
+    @Autowired
+    PatientRepository patientRepository;
+
     public void createMedicalRecord(PatientEntity patient) {
-        verifyPatientIdExists(patient.getId());
+        verifyPatientExists(patient.getId());
         MedicalRecordEntity medicalRecord = new MedicalRecordEntity(patient);
         medicalRecordsRepository.save(medicalRecord);
     }
@@ -46,7 +50,7 @@ public class MedicalRecordService {
     }
 
     public List<MedicalRecordResponseDto> listMedicalRecords(Long id) {
-        verifyPatientIdExists(id);
+        verifyPatientHasMedicalRecord(id);
         List<MedicalRecordEntity> medicalRecords = medicalRecordsRepository
                 .findAllByPatientId(id);
 
@@ -57,7 +61,7 @@ public class MedicalRecordService {
     }
 
     public List<MedicalRecordResponseDto> listMedicalRecords(Long id, String name) {
-        verifyPatientIdExists(id);
+        verifyPatientHasMedicalRecord(id);
         List<MedicalRecordEntity> medicalRecords = medicalRecordsRepository
                 .findAllByPatientIdAndPatientFullNameContainingIgnoringCase(id, name);
 
@@ -68,56 +72,62 @@ public class MedicalRecordService {
     }
 
     public void addQueriesToPatient(QueryEntity query, Long patientId) {
-        verifyPatientIdExists(patientId);
+        verifyPatientHasMedicalRecord(patientId);
         MedicalRecordEntity medicalRecord = medicalRecordsRepository.findAllByPatientId(patientId).get(0);
         medicalRecord.getQueries().add(query);
         medicalRecordsRepository.save(medicalRecord);
     }
 
     public void addDietToPatient(DietEntity diet, Long patientId) {
-        verifyPatientIdExists(patientId);
+        verifyPatientHasMedicalRecord(patientId);
         MedicalRecordEntity medicalRecord = medicalRecordsRepository.findAllByPatientId(patientId).get(0);
         medicalRecord.getDiets().add(diet);
         medicalRecordsRepository.save(medicalRecord);
     }
 
     public void addExamToPatient(ExamEntity exam, Long patientId) {
-        verifyPatientIdExists(patientId);
+        verifyPatientHasMedicalRecord(patientId);
         MedicalRecordEntity medicalRecord = medicalRecordsRepository.findAllByPatientId(patientId).get(0);
         medicalRecord.getExams().add(exam);
         medicalRecordsRepository.save(medicalRecord);
     }
 
     public void addExerciseToPatient(ExerciseEntity exercise, Long patientId) {
-        verifyPatientIdExists(patientId);
+        verifyPatientHasMedicalRecord(patientId);
         MedicalRecordEntity medicalRecord = medicalRecordsRepository.findAllByPatientId(patientId).get(0);
         medicalRecord.getExercises().add(exercise);
         medicalRecordsRepository.save(medicalRecord);
     }
 
+    public void delete(Long patientId) {
+        verifyPatientHasMedicalRecord(patientId);
+        MedicalRecordEntity medicalRecord = medicalRecordsRepository.findAllByPatientId(patientId).get(0);
+        medicalRecordsRepository.delete(medicalRecord);
+    }
+
     public void deleteQueryFromPatient(QueryEntity query, Long patientId) {
-        verifyPatientIdExists(patientId);
+        verifyPatientHasMedicalRecord(patientId);
         MedicalRecordEntity medicalRecord = medicalRecordsRepository.findAllByPatientId(patientId).get(0);
         medicalRecord.getQueries().remove(query);
         medicalRecordsRepository.save(medicalRecord);
     }
 
     public void deleteDietFromPatient(DietEntity diet, Long patientId) {
-        verifyPatientIdExists(patientId);
+        verifyPatientHasMedicalRecord(patientId);
         MedicalRecordEntity medicalRecord = medicalRecordsRepository.findAllByPatientId(patientId).get(0);
         medicalRecord.getDiets().remove(diet);
         medicalRecordsRepository.save(medicalRecord);
     }
 
     public void deleteExamFromPatient(ExamEntity exam, Long patientId) {
-        verifyPatientIdExists(patientId);
+        verifyPatientHasMedicalRecord(patientId);
         MedicalRecordEntity medicalRecord = medicalRecordsRepository.findAllByPatientId(patientId).get(0);
         medicalRecord.getExams().remove(exam);
         medicalRecordsRepository.save(medicalRecord);
     }
 
     public void deleteExerciseFromPatient(ExerciseEntity exercise, Long patientId) {
-        verifyPatientIdExists(patientId);
+        verifyPatientHasMedicalRecord(patientId);
         MedicalRecordEntity medicalRecord = medicalRecordsRepository.findAllByPatientId(patientId).get(0);
         medicalRecord.getExercises().remove(exercise);
         medicalRecordsRepository.save(medicalRecord);
@@ -133,14 +143,25 @@ public class MedicalRecordService {
                 "Busca não condiz com nenhum usuário");
     }
 
-    private void verifyPatientIdExists(Long id) {
-        boolean isPatientIdExists = this.medicalRecordsRepository.existsByPatientId(id);
+    private void verifyPatientExists(Long id) {
+        boolean isPatientIdExists = this.patientRepository.existsById(id);
 
         if (!isPatientIdExists) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND,
                     // "O paciente não cadastrado!"
                     "O id do paciente é inválido!");
+        }
+    }
+
+    private void verifyPatientHasMedicalRecord(Long id) {
+        boolean isPatientIdExists = this.medicalRecordsRepository.existsByPatientId(id);
+
+        if (!isPatientIdExists) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    // "O paciente não cadastrado!"
+                    "Nenhum prontuário encontrado para esse paciente!");
         }
     }
 }
